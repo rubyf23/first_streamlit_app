980176d8eb26eaef10bb961b3e011bd8d261b95f
Ruby Fathima <75705793+rubyf23@users.noreply.github.com>
2023-01-16 16:22:01 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,5 +41,5 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchall()
 #streamlit.text("Hello from Snowflake:")
 streamlit.header("The fruit load list contains:")
-streamlit.text(my_data_row)
-
+#streamlit.text(my_data_row)
+streamlit.dataframe(my_data_row)
